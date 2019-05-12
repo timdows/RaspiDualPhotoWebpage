10235aa33ccac820ade2f710bd2f2f77d59e511c
Timdows <tim@inexpro.nl>
2019-05-12 10:42:14 +0200

    Routing for screen management

--- dotnet version/ClientManagementApp/src/app/app.routing.ts
+++ dotnet version/ClientManagementApp/src/app/app.routing.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ResizePhotosComponent } from './resize-photos/resize-photos.component';
+import { ScreenManagementComponent } from './screen-management/screen-management.component';
 
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    // { path: 'dailypictures', component: DailyPictureComponent },
-    { path: 'resize', component: ResizePhotosComponent },
+    { path: 'resizephotos', component: ResizePhotosComponent },
+    { path: 'screenmanagement', component: ScreenManagementComponent },
     { path: '**', redirectTo: '/' }
 ];
 
--- dotnet version/ClientManagementApp/src/app/screen-management/screen-management.component.ts
+++ dotnet version/ClientManagementApp/src/app/screen-management/screen-management.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-screen-management',
-  templateUrl: './screen-management.component.html',
-  styleUrls: ['./screen-management.component.scss']
+	selector: 'app-screen-management',
+	templateUrl: './screen-management.component.pug',
+	styleUrls: ['./screen-management.component.scss']
 })
 export class ScreenManagementComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
